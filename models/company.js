7ad896655d4ebaea6651ac9c16a74b40d31d1df5
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-05-23 14:55:27 -0700

    moved constructWhereClause fn into company models

--- models/company.js
+++ models/company.js
@@ -89,6 +89,56 @@ class Company {
     return companiesRes.rows;
   }
 
+  /**Constructs a WHERE clause for SQL statements based on
+   * user inputs.
+   *  {
+        nameLike: 'a',
+        minEmployees: '250',
+        maxEmployees: '500'
+      }
+   * Outputs: a WHERE clause SQL statement object that can be passed to
+      query company model
+      {
+        whereClause: '"name" ILIKE $1 AND "num_employees" >= $2
+          AND "num_employees" <= $3',
+        values: ['a', 250, 500]
+      }
+   */
+  static async constructWhereClause(filteredData) {
+    const keys = Object.keys(parsedData);
+
+    let whereClause = [];
+    const values = [];
+
+    let i = 0;
+
+    while (i < keys.length) {
+      if ("nameLike" in parsedData) {
+        whereClause.push(`"name" ILIKE $${i + 1}`);
+        values.push("%" + parsedData.nameLike + "%");
+        i++;
+      }
+      if ("minEmployees" in parsedData) {
+        whereClause.push(`"num_employees" >=$${i + 1}`);
+        values.push(parsedData.minEmployees);
+        i++;
+      }
+      if ("maxEmployees" in parsedData) {
+        whereClause.push(`"num_employees" <=$${i + 1}`);
+        values.push(parsedData.maxEmployees);
+        i++;
+      }
+    }
+
+    whereClause = whereClause.join(" AND ");
+
+    return {
+      whereClause,
+      values
+    };
+
+  }
+
   /** Given a company handle, return data about company.
    *
    * Returns { handle, name, description, numEmployees, logoUrl, jobs }
