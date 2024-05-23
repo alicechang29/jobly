959d3af47f321b0012dafbbb54129df75ddcd8cd
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-05-23 16:12:35 -0700

    updated tests

--- helpers/parseReqQuery.test.js
+++ helpers/parseReqQuery.test.js
@@ -1,5 +1,5 @@
 import { describe, test, expect } from "vitest";
-
+import { BadRequestError } from "../expressError.js";
 import { parseReqQuery } from "./parseReqQuery.js";
 
 const reqQuery = {
@@ -21,9 +21,23 @@ describe("parseReqQuery", function () {
       }
     );
 
-    expect(typeof parsedObj.nameLike).toBe('string');
-    expect(typeof parsedObj.minEmployees).toBe('number');
-    expect(typeof parsedObj.maxEmployees).toBe('number');
+  });
+
+  test("400 if min input value is > than max input value", function () {
+    const data = {
+      nameLike: "dish net",
+      minEmployees: "50",
+      maxEmployees: "10"
+    };
+
+    try {
+      parseReqQuery(data);
+      throw new Error("fail test, you shouldn't get here");
+
+    } catch (err) {
+      expect(err instanceof BadRequestError).toBeTruthy();
+      expect(err.message).toEqual("min needs to be less than max input");
+    }
 
   });
 
