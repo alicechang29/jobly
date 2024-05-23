d1a8f02043b6a433e9775ef593ab13835e33140d
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-05-23 15:05:02 -0700

    removed whereClauseConstructor and updated docstrings

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,13 +1,13 @@
 import { BadRequestError } from "../expressError.js";
-import { parseReqQuery } from "./parseReqQuery.js";
 
 
 /** Given an object, dataToUpdate, ({firstName: 'Aliya', age: 32})
  * and an an object, jsToSql, containing the conversion of variable names
  * ({first_name: firstName}), matching snake_case to camelCase.
  *
- * Converts each key's value within dataToUpdate into a parameterized query
- * variable to protect against SQL injection.
+ * Creates an object where each key's value from dataToUpdate is turned
+ * into a parameterized query for use inside a prepared set statement
+ * along with an array of values to match to protect against SQL injection.
  *
  * Returns an object with:
     {
@@ -32,58 +32,5 @@ function sqlForPartialUpdate(dataToUpdate, jsToSql) {
 }
 
 
-/**Constructs a WHERE clause for SQL statements based on
- * user inputs.
- *  {
-      nameLike: 'a',
-      minEmployees: '250',
-      maxEmployees: '500'
-    }
- * Outputs: a WHERE clause SQL statement object that can be passed to
-    query company model
-    {
-      whereClause: '"name" ILIKE $1 AND "num_employees" >= $2
-        AND "num_employees" <= $3',
-      values: ['a', 250, 500]
-    }
 
- */
-function constructWhereClause(reqQuery) {
-
-  const parsedData = parseReqQuery(reqQuery);
-
-  const keys = Object.keys(parsedData);
-
-  let whereClause = [];
-  const values = [];
-
-  let i = 0;
-
-  while (i < keys.length) {
-    if ("nameLike" in parsedData) {
-      whereClause.push(`"name" ILIKE $${i + 1}`);
-      values.push("%" + parsedData.nameLike + "%");
-      i++;
-    }
-    if ("minEmployees" in parsedData) {
-      whereClause.push(`"num_employees" >=$${i + 1}`);
-      values.push(parsedData.minEmployees);
-      i++;
-    }
-    if ("maxEmployees" in parsedData) {
-      whereClause.push(`"num_employees" <=$${i + 1}`);
-      values.push(parsedData.maxEmployees);
-      i++;
-    }
-  }
-
-  whereClause = whereClause.join(" AND ");
-
-  return {
-    whereClause,
-    values
-  };
-
-}
-
-export { sqlForPartialUpdate, constructWhereClause };
+export { sqlForPartialUpdate };
